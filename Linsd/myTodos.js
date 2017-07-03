e4437c78976b5815528bab7c2f2aae4b65521122
lsdlinshunda <923076444@qq.com>
2017-07-04 00:50:10 +0800

    一波优化
    -移除了多余的缩进
    -加入了对空输入的处理
    -用removeClass和addClass替换原本使用的attr方法
    -用closest('li')替换parent().parent()
    -优化“删除所有已完成事项”的实现
    -实现了对事项进行修改的功能
    -更新readme

--- Linsd/myTodos.js
+++ Linsd/myTodos.js
@@ -1,60 +1,76 @@
-		var $list = $('.todo-list')
-		var $input = $('.new-todo')
-		var $count = $('.todo-count')
-		var leftItem = 0
+var $list = $('.todo-list')
+var $input = $('.new-todo')
+var $count = $('.todo-count')
+var leftItem = 0
 		
-		//add new todo item
-		$(document).keypress(function (e){ 
-		  var code = event.keyCode; 
-		  if (13 == code && $input.is(":focus")) {
-			var value = $input.val()
-			$list.append('<li class="uncompleted"><div class="view"><input class="toggle" type="checkbox" name="todo-check"><label>' + value + '</label><button class="destroy"></button></div></li>')
-			//update the number of left items
-			leftItem++
-			$count.html('<strong>'+leftItem+'</strong> item left')
-		  } 
-		})
+//add new todo item
+$(document).keypress(function (e){ 
+var code = event.keyCode; 
+if (13 == code && $input.is(":focus")) {   //bind enter event 
+	var value = $input.val()
+	if (value!==""){
+		$list.append('<li class="uncompleted"><div class="view"><input class="toggle" type="checkbox" name="todo-check"><label class="item">' + value + '</label><button class="destroy"></button></div></li>')
 		
-		//delete a todo item
-		$(document).on('click', '.destroy', function(e){
-			if ($(e.target).parent().parent().attr('class') == 'uncompleted'){
-				leftItem--
+		//update the number of left items
+		leftItem++
+		$count.html('<strong>'+leftItem+'</strong> item left')
+	} 
+}
+})
+
+//delete a todo item
+$(document).on('click', '.destroy', function(e){
+	var li=$(e.target).closest("li")   //the object need to be deleted
+    if (li.hasClass("uncompleted")){
+		leftItem--;
+		$count.html('<strong>'+leftItem+'</strong> item left')
+    }
+	li.remove();
+})
+		
+//change the state of item
+$(document).on('click', '.toggle', function(e){
+	var li=$(e.target).closest("li")
+	if(li.hasClass('completed')){
+		li.removeClass('completed')
+		li.addClass('uncompleted')
+		leftItem++
+		$count.html('<strong>'+leftItem+'</strong> item left')
+	}
+	else{
+		li.removeClass('uncompleted')
+		li.addClass('completed')
+		leftItem--
+		$count.html('<strong>'+leftItem+'</strong> item left')
+	}	
+}) 
+	
+//clear completed items
+$(document).on('click', '.clear-completed', function(e){
+	$('.completed').each(function () {
+		$(this).remove()
+	})
+})
+
+//update items
+$(document).on('click', 'label', function(e){
+	var label = $(this)
+	var txt = label.text()
+	var input = $("<input class='changeItem' type='text' value="+txt+">")
+	label.html(input)
+	input.click(function(){return false})
+	input.trigger("focus")
+	input.blur(function(){
+		var newText = $(this).val()
+		if (newText==""){                  //remove item when the text is null
+			var li=$(this).closest('li')
+			if (li.hasClass("uncompleted")){
+				leftItem--;
 				$count.html('<strong>'+leftItem+'</strong> item left')
 			}
-			$(e.target).parent().parent().remove()
-		})
-		
-		//change the state of item
-		$(document).on('click', '.toggle', function(e){
-		if($(e.target).parent().parent().attr('class')== 'completed'){
-			$(e.target).parent().parent().attr('class', 'uncompleted')
-			leftItem++
-			$count.html('<strong>'+leftItem+'</strong> item left')
+			li.remove()
 		}
-		else{
-			$(e.target).parent().parent().attr('class', 'completed')
-			leftItem--
-			$count.html('<strong>'+leftItem+'</strong> item left')
-		}
-		}) 
-		
-		//clear completed items
-		$(document).on('click', '.clear-completed', function(e){
-			$('input:checkbox').each(function () {
-				if ($(this).parent().parent().prop('class') == 'completed')
-					$(this).parent().parent().remove()
-			})
-		})
-		
-		$("label").dblclick(function(){
-			var label = $(this)
-			var txt = label.text()
-			var input = $("<input class='changeItem' type='text' value="+txt+">")
-			label.html(input)
-			input.click(function(){return false})
-			input.trigger("focus")
-			input.blur(function(){
-				var newText = $(this).val()
-				label.html(newText)
-			})
-		}) 
+		else
+			label.html(newText)
+	})
+}) 
